6fb3b7a0988f09d6e9090e0e10c06dda1c0e3896
engenharia@deltafire.com.br <Dev@4000*>
2023-02-23 09:47:25 -0300

    removida função recursiva

--- Program.cs
+++ Program.cs
@@ -1,21 +1,4 @@
-﻿// Solução incompleta
-Dictionary<int, long> _memorization = new Dictionary<int, long>();
-
-long FibMemorization(int n)
-{
-    if (_memorization.ContainsKey(n))
-        return _memorization[n];
-
-
-    if (n <= 2)
-        return 1;
-
-    _memorization[n] = FibMemorization(n - 1) + FibMemorization(n - 2);
-    return _memorization[n];
-}
-
-// Solução que funciona
-void Fib(int n)
+﻿void Fib(int n)
 {
     int previous = 0;
     int current = 1;
